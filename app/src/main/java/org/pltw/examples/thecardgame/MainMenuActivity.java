6d46737642b561ac794d18e941d00b429a8107c9
Cory Emlen <18emlcor@student.mcpsmt.org>
2018-04-18 10:18:57 -0600

    Added Card images and worked a bit on MainMenu layout

--- app/src/main/java/org/pltw/examples/thecardgame/MainMenuActivity.java
+++ app/src/main/java/org/pltw/examples/thecardgame/MainMenuActivity.java
@@ -3,7 +3,7 @@ package org.pltw.examples.thecardgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +11,3 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
     }
 }
- 
