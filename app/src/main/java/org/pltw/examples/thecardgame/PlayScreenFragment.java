093a1691c01cee22a72a29c78499c31d9ea31a0a
Ninja-Goose <18coojes@student.mcpsmt.org>
2018-04-25 11:04:19 -0600

    Set up basic play screen and settings fragment layout

--- app/src/main/java/org/pltw/examples/thecardgame/PlayScreenFragment.java
+++ app/src/main/java/org/pltw/examples/thecardgame/PlayScreenFragment.java
@@ -29,6 +29,33 @@ public class PlayScreenFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_play_screen, container, false);
+
+        playScreenTitle = v.findViewById(R.id.play_title_text);
+        againstABotButton = v.findViewById(R.id.against_a_bot);
+        oneDeviceButton = v.findViewById(R.id.two_on_one_device);
+        internetButton = v.findViewById(R.id.on_the_internet);
+
+        againstABotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: start game screen activity with bot option
+            }
+        });
+
+        oneDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: start game screen with two on one device option
+            }
+        });
+
+        internetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: start matchmaking service, start game screen with internet opt.
+            }
+        });
+
         return v;
     }
 
