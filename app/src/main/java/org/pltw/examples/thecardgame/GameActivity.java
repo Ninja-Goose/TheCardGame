43d7f61d35407e842b88603554dfeb069c6f2b51
Cory Emlen <18emlcor@student.mcpsmt.org>
2018-04-25 10:52:54 -0600

    created gameplay branch

--- app/src/main/java/org/pltw/examples/thecardgame/GameActivity.java
+++ app/src/main/java/org/pltw/examples/thecardgame/GameActivity.java
@@ -3,11 +3,12 @@ package org.pltw.examples.thecardgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity { //Main gameplay logic
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        
     }
 }
